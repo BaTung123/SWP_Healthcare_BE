f2a48c03296c6b03ce99a1d05239a2877aa341a1
BaTung <tunghoang5161997@gmail.com>
2025-08-03 10:38:27 +0700

    update DTO

--- BloodDonationSupportSystem/BDSS.DTOs/Blog/BlogDTOs.cs
+++ BloodDonationSupportSystem/BDSS.DTOs/Blog/BlogDTOs.cs
@@ -7,7 +7,6 @@ public class BlogDto
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; }
-
     public DateTime UpdatedAt { get; set; }
 }
 
